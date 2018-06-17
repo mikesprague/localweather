2bbb7a6830df572f39e916e86ba3c50a1e6071f7
Mike Sprague <mikesprague@gmail.com>
2018-06-17 13:01:36 -0400

    :recycle: Move service worker install into init module

--- src/js/modules/init.js
+++ src/js/modules/init.js
@@ -5,6 +5,20 @@ import * as data from './data';
 import * as ui from './ui';
 
 export function init() {
+  // register service worker
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', async () => {
+      const registration = await navigator.serviceWorker.register('/service-worker.js', {
+        scope: '/',
+      });
+      // registration was successful
+      console.log(`[SW] Registration successful with scope: ${registration.scope} `);
+      registration.onupdatefound = () => {
+        console.log('[SW] Update found, refresh browser to load it');
+      };
+    });
+  }
+
   cache.initCache();
   data.getLocationAndPopulateAppData();
   data.initDataUpdateCheck();
