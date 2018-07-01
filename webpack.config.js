6143de0f4c1eabe2d2bc63d023fa8de4ae09f153
Mike Sprague <mikesprague@gmail.com>
2018-07-01 17:10:13 -0400

    :building_construction: Update build system and reorganize

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
+const path = require('path');
+const variables = require('./src/js/modules/defaults');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: [
@@ -6,7 +10,8 @@ module.exports = {
     './src/scss/styles.scss',
   ],
   output: {
-    filename: './dist/js/bundle.js'
+    filename: './js/bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   devServer: {
     compress: true,
@@ -40,6 +45,7 @@ module.exports = {
           {
             loader: 'sass-loader',
             options: {
+              minimize: true,
               sourceMap: true,
             }
           }
@@ -49,7 +55,44 @@ module.exports = {
   },
   plugins: [
     new ExtractTextPlugin({
-      filename: './dist/css/styles.css',
+      filename: './css/styles.css',
     }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/index.html',
+      appName: variables.appName,
+      author: variables.author,
+      canonical: variables.canonical,
+      description: variables.description,
+      keywords: variables.keywords,
+      loadingText: variables.loadingText,
+      themeColor: variables.themeColor,
+      title: variables.title,
+      versionString: variables.versionString,
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/offline.html',
+      filename: './offline.html',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/manifest.json',
+      filename: './manifest.json',
+      appName: variables.appName,
+      canonical: variables.canonical,
+      description: variables.description,
+      themeColor: variables.themeColor,
+      title: variables.title,
+    }),
+    new CopyWebpackPlugin([{
+      from: './src/service-worker.js',
+      to: './service-worker.js',
+      force: true,
+    }]),
+    new CopyWebpackPlugin([{
+      from: './assets/**/*',
+      to: './assets/',
+    }]),
   ]
 };
