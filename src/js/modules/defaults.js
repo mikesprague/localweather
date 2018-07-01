62840505576be4a8d94da47a2b7e0936d00a3c2f
Mike Sprague <mikesprague@gmail.com>
2018-07-01 17:22:23 -0400

    :recycle: Refactor defaults and add new ones used in build

--- src/js/modules/defaults.js
+++ src/js/modules/defaults.js
@@ -1,16 +1,27 @@
 'use strict';
 
-export const cacheTimeKey = 'cacheTime';
-export const cacheTimeSpan = 600; // 10 minutes (number of minutes * 60 seconds)
-export const errorMessageSelector = '.error-message';
-export const hideClassName = 'hide-me';
-export const isOnline = false;
-export const lat = 0;
-export const lng = 0;
-export const loadFromCache = false;
-export const loadingSpinnerSelector = '.loading-spinner';
-export const locationDataKey = 'locationData';
-export const locationName = '...loading...';
-export const timerHandle = 0;
-export const title = 'LocalWeather.io (powered by Dark Sky)';
-export const weatherDataKey = 'weatherData';
+module.exports = {
+  appName: 'LocalWeather.io',
+  author: 'Michael Sprague',
+  cacheTimeKey: 'cacheTime',
+  cacheTimeSpan: 600, // 10 minutes (number of minutes * 60 seconds)
+  canonical: 'https://localweather.io/',
+  description: 'Minimalist local weather app powered by Dark Sky',
+  env: 'prod',
+  errorMessageSelector: '.error-message',
+  hideClassName: 'hide-me',
+  isOnline: false,
+  keywords: 'weather, local, dark sky, localweather.io, local weather',
+  lat: 0,
+  lng: 0,
+  loadFromCache: false,
+  loadingSpinnerSelector: '.loading-spinner',
+  loadingText: '... beep ... boop ... beep ... boop ...',
+  locationDataKey: 'locationData',
+  locationName: '',
+  themeColor: '#133150',
+  timerHandle: 0,
+  title: 'LocalWeather.io (powered by Dark Sky)',
+  versionString: 'v0.13.5',
+  weatherDataKey: 'weatherData',
+};
