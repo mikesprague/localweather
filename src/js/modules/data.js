a3265bfd059eb192147129dc582a4628f426b4c0
Michael Sprague <mikesprague@gmail.com>
2018-07-28 14:20:06 -0400

    :construction: WIP - basic offline detection

--- src/js/modules/data.js
+++ src/js/modules/data.js
@@ -7,90 +7,102 @@ import * as app from './init';
 
 export async function getLocationNameFromLatLng(lat, lng) {
   const url = `https://mikesprague-api.glitch.me/location-name/?lat=${lat}&lng=${lng}`;
-  if (defaults.loadFromCache) {
-    const cachedLocationData = cache.getData(defaults.locationDataKey);
-    defaults.locationName = cachedLocationData.results[0].formatted_address;
-    return cachedLocationData.results[0].formatted_address;
+  if (defaults.isOnline) {
+    if (defaults.loadFromCache) {
+      const cachedLocationData = cache.getData(defaults.locationDataKey);
+      defaults.locationName = cachedLocationData.results[0].formatted_address;
+      return cachedLocationData.results[0].formatted_address;
+    } else {
+      const locationData = fetch(url)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            console.error(response);
+          }
+        })
+        .then(json => {
+          cache.setData(defaults.locationDataKey, json);
+          defaults.locationName = json.results[0].formatted_address;
+          return json.results[0].formatted_address;
+        })
+        .catch(error => {
+          console.error(`Error in getLocationNameFromLatLng:\n ${error.message}`);
+        });
+      return locationData;
+    }
   } else {
-    const locationData = fetch(url)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          console.error(response);
-        }
-      })
-      .then(json => {
-        cache.setData(defaults.locationDataKey, json);
-        defaults.locationName = json.results[0].formatted_address;
-        return json.results[0].formatted_address;
-      })
-      .catch(error => {
-        console.error(`Error in getLocationNameFromLatLng:\n ${error.message}`);
-      });
-    return locationData;
+    location.replace('/offline.html');
   }
 }
 
 export async function getWeather(lat, lng) {
-  const url = `https://mikesprague-api.glitch.me/weather/?lat=${lat}&lng=${lng}`;
-  if (defaults.loadFromCache) {
-    const cachedWeatherData = cache.getData(defaults.weatherDataKey);
-    return cachedWeatherData;
+  if (defaults.isOnline) {
+    const url = `https://mikesprague-api.glitch.me/weather/?lat=${lat}&lng=${lng}`;
+    if (defaults.loadFromCache) {
+      const cachedWeatherData = cache.getData(defaults.weatherDataKey);
+      return cachedWeatherData;
+    } else {
+      const weatherData = fetch(url)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            console.error(response);
+          }
+        })
+        .then(json => {
+          cache.setData(defaults.weatherDataKey, json);
+          return json;
+        })
+        .catch(error => {
+          console.error(`Error in getWeather:\n ${error.message}`);
+          ui.hideLoading();
+        });
+      return weatherData;
+    }
   } else {
-    const weatherData = fetch(url)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          console.error(response);
-        }
-      })
-      .then(json => {
-        cache.setData(defaults.weatherDataKey, json);
-        return json;
-      })
-      .catch(error => {
-        console.error(`Error in getWeather:\n ${error.message}`);
-        ui.hideLoading();
-      });
-    return weatherData;
+    location.replace('/offline.html');
   }
 }
 
 export async function getLocationAndPopulateAppData() {
-  ui.showLoading();
-  if (defaults.loadFromCache) {
-    const cachedLocationData = cache.getData(defaults.locationDataKey);
-    defaults.locationName = cachedLocationData.results[0].formatted_address;
-    const cachedWeatherData = cache.getData(defaults.weatherDataKey);
-    ui.renderAppWithData(cachedWeatherData);
-    ui.hideLoading();
-  } else {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(position => {
-        defaults.lat = position.coords.latitude;
-        defaults.lng = position.coords.longitude;
-        getLocationNameFromLatLng(
-          defaults.lat,
-          defaults.lng
-        ).then(name => {
-          defaults.locationName = name;
-          getWeather(
+  if (defaults.isOnline) {
+    ui.showLoading();
+    if (defaults.loadFromCache) {
+      const cachedLocationData = cache.getData(defaults.locationDataKey);
+      defaults.locationName = cachedLocationData.results[0].formatted_address;
+      const cachedWeatherData = cache.getData(defaults.weatherDataKey);
+      ui.renderAppWithData(cachedWeatherData);
+      ui.hideLoading();
+    } else {
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(position => {
+          defaults.lat = position.coords.latitude;
+          defaults.lng = position.coords.longitude;
+          getLocationNameFromLatLng(
             defaults.lat,
             defaults.lng
-          ).then(json => {
-            ui.renderAppWithData(json);
-          }).then(() => {
-            ui.hideLoading();
+          ).then(name => {
+            defaults.locationName = name;
+            getWeather(
+              defaults.lat,
+              defaults.lng
+            ).then(json => {
+              ui.renderAppWithData(json);
+            }).then(() => {
+              ui.hideLoading();
+            });
+          }).catch(error => {
+            cosole.error(`ERROR: ${error}`);
           });
-        }).catch(error => {
-          cosole.error(`ERROR: ${error}`);
         });
-      });
-    } else {
-      console.error('ERROR: Your browser must support geolocation and you must approve sharing your location with the site for the app to work')
+      } else {
+        console.error('ERROR: Your browser must support geolocation and you must approve sharing your location with the site for the app to work')
+      }
     }
+  } else {
+    location.replace('/offline.html');
   }
 }
 
