763aa50033204bdbcacd09b685dbdbb3dac5620d
Michael Sprague <mikesprague@gmail.com>
2018-07-28 14:20:06 -0400

    :construction: WIP - basic offline detection

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,14 @@
 
 import * as app from './modules/init';
 
+window.addEventListener('offline', () => {
+  location.replace('/offline.html')
+});
+
+window.addEventListener('online', () => {
+  location.replace('/')
+});
+
 document.onreadystatechange = function () {
   if (document.readyState === "interactive") {
     app.init();
