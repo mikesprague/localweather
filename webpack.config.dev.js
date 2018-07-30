b2deb4011e3a0b22841c06fc114d77084b71f027
Michael Sprague <mikesprague@gmail.com>
2018-07-30 18:49:18 -0400

    :wrench: Add dev config for webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,9 +3,6 @@ const variables = require('./src/js/modules/defaults');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: [
@@ -16,15 +13,23 @@ module.exports = {
     filename: './js/bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  mode: 'production',
+  devServer: {
+    compress: true,
+    port: 5500,
+    overlay: {
+      warnings: true,
+      errors: true
+    }
+  },
+  mode: 'development',
   module: {
     rules: [
       {
           test: /\.s?[ac]ss$/,
           use: [
               MiniCssExtractPlugin.loader,
-              { loader: 'css-loader', options: { url: false, sourceMap: true } },
-              { loader: 'sass-loader', options: { sourceMap: true } }
+              { loader: 'css-loader' },
+              { loader: 'sass-loader' }
           ],
       },
       {
@@ -33,21 +38,7 @@ module.exports = {
       }
     ]
   },
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        cache: false,
-        parallel: true,
-        sourceMap: false,
-        uglifyOptions: {
-          compress: false
-        },
-      }),
-      new OptimizeCSSAssetsPlugin({}),
-    ]
-  },
   plugins: [
-    new CleanWebpackPlugin('dist', {}),
     new MiniCssExtractPlugin({
       filename: './css/styles.css',
     }),
