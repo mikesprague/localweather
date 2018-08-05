f50137f5d23443d94c9056050f4114be505ba918
Michael Sprague <mikesprague@gmail.com>
2018-08-05 11:40:11 -0400

    :bug: Fix syntax for exports in defaults module

--- src/js/modules/defaults.js
+++ src/js/modules/defaults.js
@@ -1,32 +1,32 @@
 'use strict';
 
-export appName = 'LocalWeather.io';
-export author = 'Michael Sprague';
-export cacheTimeKey = 'cacheTime';
-export cacheTimeSpan = 600; // 10 minutes (number of minutes * 60 seconds)
-export canonical = 'https = /localweather.io/';
-export coordsDataKey = 'coordsData';
-export description = 'Minimalist local weather app powered by Dark Sky';
-export errorMessageSelector = '.error-message';
-export hideClassName = 'hide-me';
-export keywords = 'weather, local, dark sky, localweather.io, local weather';
-export lat = 0;
-export lng = 0;
-export loadFromCache = false;
-export loadingSpinnerSelector = '.loading-spinner';
-export loadingText = '... loading weather data for your location ...';
-export locationDataKey = 'locationData';
-export locationName = '';
-export offlineHeading = 'You appear to be offline';
-export offlineText = 'This page will automatically update with current weather data when you have a stable connection again';
-export themeColor = '#133150';
-export timerHandle = 0;
-export title = 'LocalWeather.io (powered by Dark Sky)';
-export versionString = 'v0.19.3';
-export weatherDataKey = 'weatherData';
-export apiUrl = function () {
+exports.appName = 'LocalWeather.io';
+exports.author = 'Michael Sprague';
+exports.cacheTimeKey = 'cacheTime';
+exports.cacheTimeSpan = 600; // 10 minutes (number of minutes * 60 seconds)
+exports.canonical = 'https = /localweather.io/';
+exports.coordsDataKey = 'coordsData';
+exports.description = 'Minimalist local weather app powered by Dark Sky';
+exports.errorMessageSelector = '.error-message';
+exports.hideClassName = 'hide-me';
+exports.keywords = 'weather, local, dark sky, localweather.io, local weather';
+exports.lat = 0;
+exports.lng = 0;
+exports.loadFromCache = false;
+exports.loadingSpinnerSelector = '.loading-spinner';
+exports.loadingText = '... loading weather data for your location ...';
+exports.locationDataKey = 'locationData';
+exports.locationName = '';
+exports.offlineHeading = 'You appear to be offline';
+exports.offlineText = 'This page will automatically update with current weather data when you have a stable connection again';
+exports.themeColor = '#133150';
+exports.timerHandle = 0;
+exports.title = 'LocalWeather.io (powered by Dark Sky)';
+exports.versionString = 'v0.19.2';
+exports.weatherDataKey = 'weatherData';
+exports.apiUrl = function () {
   return window.location.hostname === 'localhost' ? 'http://localhost:3000' : 'https://api.localweather.io';
 };
-export isOnline = function () {
+exports.isOnline = function () {
   return navigator.onLine;
 };
