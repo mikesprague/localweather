6fabfd622812d47b2abb7fe3fec4d33c50487b01
Michael Sprague <mikesprague@gmail.com>
2018-08-12 16:12:15 -0400

    :bookmark: Bump version version v0.21.3

--- src/js/modules/defaults.js
+++ src/js/modules/defaults.js
@@ -22,7 +22,7 @@ module.exports = {
   themeColor: '#133150',
   timerHandle: 0,
   title: 'LocalWeather.io (powered by Dark Sky)',
-  versionString: 'v0.21.2',
+  versionString: 'v0.21.3',
   weatherDataKey: 'weatherData',
   apiUrl: function () {
     return window.location.hostname === 'localhost' ? 'http://localhost:3000' : 'https://api.localweather.io';
