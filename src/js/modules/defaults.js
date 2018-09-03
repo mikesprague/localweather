ef993e06800a07b3d8b412724407400d4f9099ef
Michael Sprague <mikesprague@gmail.com>
2018-09-03 16:59:35 -0400

    :bookmark: Bump version v0.25.0
    Also adds missed rollbar -> bugsnag call conversion

--- src/js/modules/defaults.js
+++ src/js/modules/defaults.js
@@ -27,7 +27,7 @@ module.exports = {
   themeColor: "#133150",
   timerHandle: 0,
   title: "LocalWeather.io (powered by Dark Sky)",
-  versionString: "v0.24.4",
+  versionString: "v0.25.0",
   weatherDataKey: "weatherData",
   apiUrl: function () {
     return window.location.hostname === "localhost" ? "http://localhost:3000" : "https://api.localweather.io";
