099612cde517c8281103b7a1c78e4071743df08d
Michael Sprague <mikesprague@gmail.com>
2018-11-18 13:15:35 -0500

    :sparkles: Add lambda functions to replace current Dockerized API

---
+++ functions/weather.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const request = require("request");
+const rp = require("request-promise");
+
+exports.handler = function(event, context, callback) {
+  const lat = event.queryStringParameters.lat;
+  const lng = event.queryStringParameters.lng;
+  if (!lat) {
+    callback(null, {
+      statusCode: 400,
+      body: "Missing 'lat' parameter",
+    });
+  }
+  if (!lng) {
+    callback(null, {
+      statusCode: 400,
+      body: "Missing 'lng' parameter",
+    });
+  }
+  const apiUrlToCall = `https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/${lat},${lng}/?exclude=[minutely]`;
+  const rpOptions = {
+    uri: apiUrlToCall,
+    headers: {
+        "User-Agent": "Request-Promise"
+    },
+    json: true
+  };
+  rp(rpOptions)
+  .then(body => {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(body),
+    });
+  })
+  .catch(err => {
+    callback(new Error(err), {
+      statusCode: 500,
+      body: JSON.stringify(err),
+    });
+  });
+};
