e1ad165ba5d16df7aedbe1b06557ae8ab810dff1
Michael Sprague <mikesprague@gmail.com>
2018-11-18 15:54:06 -0500

    :loud_sound: Add Bugsnag dependency and use for errors in lambda functions

--- src/functions/location-name.js
+++ src/functions/location-name.js
@@ -2,6 +2,9 @@
 
 const request = require("request");
 const rp = require("request-promise");
+const bugsnag = require("bugsnag");
+
+bugsnag.register(`${process.env.BUGSNAG_KEY}`);
 
 exports.handler = function(event, context, callback) {
   const lat = event.queryStringParameters.lat;
@@ -34,7 +37,7 @@ exports.handler = function(event, context, callback) {
     });
   })
   .catch(err => {
-    callback(new Error(err), {
+    callback(bugsnag.notify(new Error(err)), {
       statusCode: 500,
       body: JSON.stringify(err),
     });
