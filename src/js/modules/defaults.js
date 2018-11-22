f33e5f0638a7a7bf583deb3cf78f79fcf9df0337
Michael Sprague <mikesprague@gmail.com>
2018-11-22 12:08:05 -0500

    :bookmark: Bump version v0.37.0

--- src/js/modules/defaults.js
+++ src/js/modules/defaults.js
@@ -24,7 +24,7 @@ module.exports = {
   themeColor: "#133150",
   timerHandle: 0,
   title: "LocalWeather.io (powered by Dark Sky)",
-  versionString: "v0.36.0",
+  versionString: "v0.37.0",
   weatherDataKey: "weatherData",
   apiUrl: function () {
     return window.location.hostname === "localhost" ? "http://localhost:9000" : `https://${window.location.hostname}/.netlify/functions`;
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "0.36.0";
+const VERSION = "0.37.0";
 const CACHE_NAME = `localWeather-io-${VERSION}`;
 const cacheAlways = [
   "/",
