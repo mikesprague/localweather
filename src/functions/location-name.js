9848077c4d25d66c62fb7de63bfde31c10b7d4a3
Michael Sprague <mikesprague@gmail.com>
2019-01-23 15:01:16 -0500

    :rotating_light: Fix linting errors

--- src/functions/location-name.js
+++ src/functions/location-name.js
@@ -1,51 +1,49 @@
-const request = require("request");
-const rp = require("request-promise");
-const bugsnag = require("bugsnag");
+const bugsnag = require('bugsnag').register(`${process.env.BUGSNAG_KEY}`);
+const rp = require('request-promise');
 
-bugsnag.register(`${process.env.BUGSNAG_KEY}`);
-
-exports.handler = function(event, context, callback) {
-  const lat = event.queryStringParameters.lat;
-  const lng = event.queryStringParameters.lng;
+exports.handler = (event, context, callback) => {
+  const { lat, lng } = event.queryStringParameters;
   const callbackHeaders = {
-    "Access-Control-Allow-Origin" : "*",
-    "Access-Control-Allow-Headers": "Content-Type"
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
   };
+
   if (!lat) {
     callback(null, {
       statusCode: 400,
       headers: callbackHeaders,
-      body: "Missing 'lat' parameter",
+      body: 'Missing "lat" parameter',
     });
   }
   if (!lng) {
     callback(null, {
       statusCode: 400,
       headers: callbackHeaders,
-      body: "Missing 'lng' parameter",
+      body: 'Missing "lng" parameter',
     });
   }
+
   const apiUrlToCall = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
   const rpOptions = {
     uri: apiUrlToCall,
     headers: {
-        "User-Agent": "Request-Promise"
+      'User-Agent': 'Request-Promise',
     },
-    json: true
+    json: true,
   };
   rp(rpOptions)
-  .then(body => {
-    callback(null, {
-      statusCode: 200,
-      headers: callbackHeaders,
-      body: JSON.stringify(body),
+    .then((body) => {
+      callback(null, {
+        statusCode: 200,
+        headers: callbackHeaders,
+        body: JSON.stringify(body),
+      });
+    })
+    .catch((err) => {
+      callback(bugsnag.notify(new Error(err)), {
+        statusCode: 500,
+        headers: callbackHeaders,
+        body: JSON.stringify(err),
+      });
     });
-  })
-  .catch(err => {
-    callback(bugsnag.notify(new Error(err)), {
-      statusCode: 500,
-      headers: callbackHeaders,
-      body: JSON.stringify(err),
-    });
-  });
 };
--- src/functions/weather.js
+++ src/functions/weather.js
@@ -1,53 +1,49 @@
-const request = require("request");
-const rp = require("request-promise");
-const bugsnag = require("bugsnag");
+const bugsnag = require('bugsnag').register(`${process.env.BUGSNAG_KEY}`);
+const rp = require('request-promise');
 
-bugsnag.register(`${process.env.BUGSNAG_KEY}`);
-
-exports.handler = function(event, context, callback) {
-  const lat = event.queryStringParameters.lat;
-  const lng = event.queryStringParameters.lng;
-  const units = event.queryStringParameters.units || "auto";
+exports.handler = (event, context, callback) => {
+  const { lat, lng } = event.queryStringParameters;
+  const units = event.queryStringParameters.units || 'auto';
 
   const callbackHeaders = {
-    "Access-Control-Allow-Origin" : "*",
-    "Access-Control-Allow-Headers": "Content-Type"
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
   };
   if (!lat) {
     callback(null, {
       statusCode: 400,
       headers: callbackHeaders,
-      body: "Missing 'lat' parameter",
+      body: 'Missing "lat" parameter',
     });
   }
   if (!lng) {
     callback(null, {
       statusCode: 400,
       headers: callbackHeaders,
-      body: "Missing 'lng' parameter",
+      body: 'Missing "lng" parameter',
     });
   }
   const apiUrlToCall = `https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/${lat},${lng}/?units=${units}`;
   const rpOptions = {
     uri: apiUrlToCall,
     headers: {
-        "User-Agent": "Request-Promise"
+      'User-Agent': 'Request-Promise',
     },
-    json: true
+    json: true,
   };
   rp(rpOptions)
-  .then(body => {
-    callback(null, {
-      statusCode: 200,
-      headers: callbackHeaders,
-      body: JSON.stringify(body),
+    .then((body) => {
+      callback(null, {
+        statusCode: 200,
+        headers: callbackHeaders,
+        body: JSON.stringify(body),
+      });
+    })
+    .catch((err) => {
+      callback(bugsnag.notify(new Error(err)), {
+        statusCode: 500,
+        headers: callbackHeaders,
+        body: JSON.stringify(err),
+      });
     });
-  })
-  .catch(err => {
-    callback(bugsnag.notify(new Error(err)), {
-      statusCode: 500,
-      headers: callbackHeaders,
-      body: JSON.stringify(err),
-    });
-  });
 };
