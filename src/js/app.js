82a2a416cdaae857429cc31b15fb3abb0ee803d0
Michael Sprague <mikesprague@gmail.com>
2019-01-24 12:12:15 -0500

    :boom: Add offline page and logic around it
    Will probably break things, fixes to come

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,33 @@
 import '../scss/styles.scss';
-import { init } from './modules/init';
+import { init, initOffline } from './modules/init';
 import { initFontAwesomeIcons, hasApprovedLocationSharing } from './modules/ui';
+import { isOnline } from './modules/defaults';
+
+window.addEventListener('offline', () => {
+  // TODO: add offline handler
+  console.log('Browser offline');
+  window.location.replace('/offline.html');
+}, false);
+
+window.addEventListener('online', () => {
+  // TODO: add online handler
+  console.log('Browser online');
+  window.location.replace('/');
+}, false);
 
 document.onreadystatechange = () => {
-  if (document.readyState === 'interactive') {
-    if (hasApprovedLocationSharing()) {
-      init();
-    } else {
-      initFontAwesomeIcons();
-      document.querySelector('.btn-init-app').addEventListener('click', () => {
+  if (isOnline()) {
+    if (document.readyState === 'interactive') {
+      if (hasApprovedLocationSharing()) {
         init();
-      });
+      } else {
+        initFontAwesomeIcons();
+        document.querySelector('.btn-init-app').addEventListener('click', () => {
+          init();
+        });
+      }
     }
+  } else {
+    initOffline();
   }
 };
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,64 +1,115 @@
-const canonical = "https://dev.localweather.io";
-const path = require("path");
-const variables = require("./src/js/modules/defaults");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const canonical = 'https://localweather.io';
+const path = require('path');
+const WebPackBar = require('webpackbar');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const variables = require('./src/js/modules/defaults');
 
 module.exports = {
   entry: [
-    "./src/js/app.js",
-    "./src/scss/styles.scss",
+    './src/js/app.js',
   ],
+  devtool: 'source-map',
   output: {
-    filename: "./js/bundle.js",
-    chunkFilename: "./js/[name].bundle.js",
-    path: path.resolve(__dirname, "build")
+    filename: './js/bundle.js',
+    chunkFilename: './js/[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
   },
-  devServer: {
-    compress: true,
-    port: 5500,
-    overlay: {
-      warnings: true,
-      errors: true
-    }
-  },
-  mode: "development",
+  mode: 'development',
   module: {
     rules: [{
-        test: /\.s?[ac]ss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader"
+      test: /\.s?[ac]ss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: {
+            sourceMap: true,
           },
-          {
-            loader: "sass-loader"
-          }
-        ],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-      }
-    ]
+        },
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true,
+          },
+        },
+        {
+          loader: 'postcss-loader',
+          options: {
+            plugins() {
+              return [autoprefixer({
+                browsers: 'last 3 versions',
+              })];
+            },
+          },
+        },
+      ],
+    },
+    {
+      test: /\.(js)$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  node: 'current',
+                },
+                useBuiltIns: 'entry',
+              },
+            ],
+          ],
+        },
+      }],
+    }],
   },
   optimization: {
     splitChunks: {
-      chunks: "all",
+      chunks: 'all',
     },
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        sourceMap: true,
+      }),
+      new OptimizeCSSAssetsPlugin(),
+    ],
   },
   plugins: [
+    new WebPackBar(),
     new MiniCssExtractPlugin({
-      filename: "./css/styles.css",
+      filename: './css/styles.css',
+      chunkFilename: './css/[id].css',
     }),
     new HtmlWebpackPlugin({
       inject: false,
-      template: "./src/index.html",
-      environment: "development",
+      template: './src/index.html',
+      environment: 'development',
       appName: variables.appName,
       author: variables.author,
-      canonical: canonical,
+      canonical,
+      description: variables.description,
+      keywords: variables.keywords,
+      loadingText: variables.loadingText,
+      themeColor: variables.themeColor,
+      title: variables.title,
+      versionString: variables.versionString,
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/offline.html',
+      filename: 'offline.html',
+      environment: 'development',
+      appName: variables.appName,
+      author: variables.author,
+      canonical,
       description: variables.description,
       keywords: variables.keywords,
       loadingText: variables.loadingText,
@@ -67,24 +118,24 @@ module.exports = {
       versionString: variables.versionString,
     }),
     new CopyWebpackPlugin([{
-      from: "./src/service-worker.js",
-      to: "./",
+      from: './src/service-worker.js',
+      to: './',
       force: true,
     }]),
     new CopyWebpackPlugin([{
-      from: "./src/manifest.json",
-      to: "./",
+      from: './src/manifest.json',
+      to: './',
       force: true,
     }]),
     new CopyWebpackPlugin([{
-      from: "./src/_redirects",
-      to: "./",
+      from: './src/_redirects',
+      to: './',
       force: true,
     }]),
     new CopyWebpackPlugin([{
-      from: "./assets/**/*",
-      to: "./",
+      from: './assets/**/*',
+      to: './',
       force: true,
     }]),
-  ]
+  ],
 };
