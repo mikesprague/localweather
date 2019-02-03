e7603487909455e8c8a8f4bb86bf2d04295667d2
Michael Sprague <mikesprague@gmail.com>
2019-02-03 11:38:51 -0500

    :bookmark: Bump version v0.67.0

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = '0.66.1';
+const VERSION = '0.67.0';
 const CACHE_NAME = `localWeather-io-${VERSION}`;
 const cacheAlways = [
   '.',
@@ -29,17 +29,17 @@ addEventListener('install', (installEvent) => {
   // perform install steps
   installEvent.waitUntil(
     caches.open(CACHE_NAME)
-      .then((staticCache) => {
-        // Nice to have cached
-        staticCache.addAll(cacheWhenPossible).then(() => {
-          // console.info('[SW] Cached the 'nice to haves'');
-        }); // end addAll/then
-        // Must have cached
-        return staticCache.addAll(cacheAlways).then(() => {
-          // console.info('[SW] Cached the 'must haves'');
-          // console.info(`[SW] Finished Updating LocalWeather.io ${CACHE_NAME}`);
-        }); // end return addAll/then
-      }) // end open then
+    .then((staticCache) => {
+      // Nice to have cached
+      staticCache.addAll(cacheWhenPossible).then(() => {
+        // console.info('[SW] Cached the 'nice to haves'');
+      }); // end addAll/then
+      // Must have cached
+      return staticCache.addAll(cacheAlways).then(() => {
+        // console.info('[SW] Cached the 'must haves'');
+        // console.info(`[SW] Finished Updating LocalWeather.io ${CACHE_NAME}`);
+      }); // end return addAll/then
+    }) // end open then
   ); // end waitUntil
 }); // end addEventListener
 
@@ -47,20 +47,20 @@ addEventListener('activate', (activateEvent) => {
   // console.info('[SW] Activate Started');
   activateEvent.waitUntil(
     caches.keys()
-      .then((cacheNames) => {
-        return Promise.all(
-          cacheNames
-            .filter(cacheName => cacheName !== CACHE_NAME)
-            .map((cacheName) => {
-              // console.info(`[SW] Deleted Old Version ${cacheName}`);
-              return caches.delete(cacheName);
-            }) // end map
-        ); // end return Promise.all
-      }) // end keys then
-      .then(() => {
-        // console.info('[SW] Activated');
-        return clients.claim();
-      }) // end then
+    .then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+        .filter(cacheName => cacheName !== CACHE_NAME)
+        .map((cacheName) => {
+          // console.info(`[SW] Deleted Old Version ${cacheName}`);
+          return caches.delete(cacheName);
+        }) // end map
+      ); // end return Promise.all
+    }) // end keys then
+    .then(() => {
+      // console.info('[SW] Activated');
+      return clients.claim();
+    }) // end then
   ); // end waitUntil
 }); // end addEventListener
 
