f1dee5cbc642fff5643fe7ed466a5c3f80db3d21
Michael Sprague <mikesprague@gmail.com>
2021-07-03 14:16:11 -0400

    :recycle: Refactor to use axios

--- src/functions/location-and-weather.js
+++ src/functions/location-and-weather.js
@@ -1,51 +1,62 @@
+const axios = require('axios');
 const Bugsnag = require('@bugsnag/js');
-const rp = require('request-promise');
 
-exports.handler = (event, context, callback) => {
-  Bugsnag.start(process.env.BUGSNAG_KEY);
-  const { lat, lng } = event.queryStringParameters;
+Bugsnag.start({ apiKey: process.env.BUGSNAG_API_KEY });
+
+exports.handler = async (event, context, callback) => {
+  const { lat, lng, time, healthcheck } = event.queryStringParameters || null;
   const callbackHeaders = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'Content-Type',
+    'Content-Type': 'application/json',
   };
+
+  if (healthcheck) {
+    return {
+      headers: callbackHeaders,
+      statusCode: 200,
+      body:  JSON.stringify('API is up and running'),
+    };
+  }
 
   if (!lat) {
-    callback(null, {
-      statusCode: 400,
+    return {
       headers: callbackHeaders,
-      body: 'Missing "lat" parameter',
-    });
+      statusCode: 400,
+      body:  JSON.stringify('Missing "lat" parameter'),
+    };
   }
   if (!lng) {
-    callback(null, {
-      statusCode: 400,
+    return {
       headers: callbackHeaders,
-      body: 'Missing "lng" parameter',
-    });
+      statusCode: 400,
+      body:  JSON.stringify('Missing "lng" parameter'),
+    };
   }
 
-  const units = event.queryStringParameters.units || 'auto';
-  const geocodeApiUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
-  const weatherApiUrl = `https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/${lat},${lng}/?units=${units}`;
+  const {
+    GOOGLE_MAPS_API_KEY,
+    DARK_SKY_API_KEY,
+    // OPEN_WEATHERMAP_API_KEI,
+  } = process.env;
 
-  const geocodeOptions = {
-    uri: geocodeApiUrl,
-    headers: {
-      'User-Agent': 'Request-Promise',
-    },
-    json: true,
-  };
-  const geocodePromise = rp(geocodeOptions)
+  const units = event.queryStringParameters.units || 'auto';
+  const geocodeApiUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${GOOGLE_MAPS_API_KEY}`;
+  const weatherApiUrl = `https://api.darksky.net/forecast/${DARK_SKY_API_KEY}/${lat},${lng}${time ? ',' + time : ''}/?units=${units}`;
+  // const openWeatherMapApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&&units=${units}&appid=${OPEN_WEATHERMAP_API_KEI}`;
+
+  const geocodePromise = await axios.get(geocodeApiUrl)
     .then((response) => {
-      const fullResults = response.results;
-      const formattedAddress = response.results[0].formatted_address;
+      const fullResults = response.data.results;
+      const formattedAddress = fullResults[0].formatted_address;
       let locationName = '';
-      const addressTargets = ['neighborhood', 'locality', 'administrative_area_level_2', 'administrative_area_level_1'];
-      addressTargets.map((target) => {
+      const isUSA = formattedAddress.toLowerCase().includes('usa');
+      const addressTargets = ['postal_town', 'locality', 'neighborhood', 'administrative_area_level_2', 'administrative_area_level_1', 'country'];
+      addressTargets.forEach((target) => {
         if (!locationName.length) {
-          response.results.map((result) => {
+          fullResults.forEach((result) => {
             if (!locationName.length) {
-              result.address_components.map((component) => {
+              result.address_components.forEach((component) => {
                 if (!locationName.length && component.types.indexOf(target) > -1) {
                   locationName = component.long_name;
                 }
@@ -54,64 +65,54 @@ exports.handler = (event, context, callback) => {
           });
         }
       });
-      if (locationName.length) {
-        // console.log(locationName);
-        const locationData = {
-          location: {
-            locationName,
-            formattedAddress,
-            fullResults,
-          },
-        };
-        return locationData;
-      }
+      fullResults[0].address_components.forEach((component) => {
+        if (isUSA && component.types.indexOf('administrative_area_level_1') > -1) {
+          locationName = `${locationName}, ${component.short_name}`;
+        }
+        if (!isUSA && component.types.indexOf('country') > -1) {
+          locationName = `${locationName}, ${component.short_name}`;
+        }
+      });
+      // console.log(locationName);
+      const locationData = {
+        location: {
+          locationName,
+          formattedAddress,
+          fullResults,
+        },
+      };
+      return locationData;
     })
     .catch((err) => {
       console.log(err);
-      callback(Bugsnag.notify(err), {
-        statusCode: 500,
+      return {
         headers: callbackHeaders,
+        statusCode: 500,
         body: JSON.stringify(err),
-      });
+      };
     });
 
-  const weatherOptions = {
-    uri: weatherApiUrl,
-    headers: {
-      'User-Agent': 'Request-Promise',
-    },
-    json: true,
-  };
-  const weatherPromise = rp(weatherOptions)
+  const weatherPromise = await axios.get(weatherApiUrl)
     .then((response) => {
       const weatherData = {
-        weather: response,
+        weather: response.data,
       };
       return weatherData;
     })
-    .catch((err) => {
-      console.log(err);
-      callback(Bugsnag.notify(err), {
-        statusCode: 500,
+    .catch((err) => (
+      {
         headers: callbackHeaders,
+        statusCode: 500,
         body: JSON.stringify(err),
-      });
-    });
+      }
+    ));
 
-  Promise.all(
-    [geocodePromise, weatherPromise],
-  ).then((response) => {
-    callback(null, {
-      statusCode: 200,
-      headers: callbackHeaders,
-      body: JSON.stringify(response),
-    });
-  }).catch((err) => {
-    console.log(err);
-    callback(Bugsnag.notify(err), {
-      statusCode: 500,
-      headers: callbackHeaders,
-      body: JSON.stringify(err),
-    });
-  });
+  return {
+    headers: callbackHeaders,
+    statusCode: 200,
+    body: JSON.stringify({
+      location: geocodePromise.location,
+      weather: weatherPromise.weather,
+    }),
+  };
 };
