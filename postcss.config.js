97ee4c4326f018c0bbbdcc272e0aabc7cf735174
Michael Sprague <mikesprague@gmail.com>
2024-12-01 14:10:27 -0500

    fix(build): remove unused import

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 import { purgeCSSPlugin } from '@fullhuman/postcss-purgecss';
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
-import tailwindcss from 'tailwindcss';
 
 const cssSafelistClassArray = [
   /swal2/,
@@ -22,7 +21,6 @@ const cssSafelistClassArray = [
 export default {
   plugins: [
     autoprefixer,
-    tailwindcss,
     cssnano({
       preset: 'default',
     }),
